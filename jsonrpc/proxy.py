4fdabdf30fd13dbdbb2bab1ac6354e3c83bc5ac7
David Aguilar <davvid@gmail.com>
2012-02-07 16:26:47 -0800

    PEP-8: Cleanup the coding style in proxy.py

--- jsonrpc/proxy.py
+++ jsonrpc/proxy.py
@@ -25,28 +25,28 @@ from jsonrpc.json import dumps, loads
 
 
 class JSONRPCException(Exception):
-    def __init__(self, rpcError):
+    def __init__(self, error):
         Exception.__init__(self)
-        self.error = rpcError
+        self.error = error
 
 
 class ServiceProxy(object):
-    def __init__(self, serviceURL, serviceName=None):
-        self.__serviceURL = serviceURL
-        self.__serviceName = serviceName
+    def __init__(self, url, name=None):
+        self._url = url
+        self._name = name
 
     def __getattr__(self, name):
-        if self.__serviceName != None:
-            name = '%s.%s' % (self.__serviceName, name)
-        return ServiceProxy(self.__serviceURL, name)
+        if self._name is not None:
+            name = '%s.%s' % (self._name, name)
+        return ServiceProxy(self._url, name)
 
     def __call__(self, *args):
-         postdata = dumps({'method': self.__serviceName,
+         postdata = dumps({'method': self._name,
                            'params': args,
                            'id': 'jsonrpc'})
-         respdata = urllib.urlopen(self.__serviceURL, postdata).read()
+         respdata = urllib.urlopen(self._url, postdata).read()
          resp = loads(respdata)
-         if resp['error'] != None:
+         if resp['error'] is not None:
              raise JSONRPCException(resp['error'])
          else:
              return resp['result']
