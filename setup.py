ecc9180456c8594edd76dca86ac98f7c7e80ba66
David Aguilar <davvid@gmail.com>
2012-02-07 17:34:23 -0800

    setup.py: Update project information

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 
 from distutils.core import setup
 
-setup(name = "jsonrpc",
-    version = "0.01",
-    description = "A json-rpc package which implements JSON-RPC over HTTP.",
-    keywords = "JSON RPC",
-    author = "Jan-Klaas Kollhof",
-    url = "http://json-rpc.org/wiki/python-json-rpc",
-    license = "LGPL",
-    long_description = """""",
-    packages = ['jsonrpc']
+setup(name = 'jsonrpc',
+      version = '1.0',
+      description = 'A json-rpc package which implements JSON-RPC over HTTP.',
+      keywords = 'JSON RPC',
+      author = 'David Aguilar',
+      url = 'https://github.com/davvid/python-jsonrpc',
+      license = 'LGPL',
+      long_description = """""",
+      packages = ['jsonrpc']
 )
