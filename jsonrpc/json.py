6e427cae32366395c17ce757eb9d8573faba0ae6
David Aguilar <davvid@gmail.com>
2012-06-21 13:19:38 -0700

    json: Automatically convert datetime objects to ISO format
    Convert datetime objects to ISO format strings when serializing.
    This allows us to send responses that include datetime objects.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- jsonrpc/json.py
+++ jsonrpc/json.py
@@ -1,10 +1,25 @@
+from datetime import datetime
+
 try:
     json = __import__('simplejson')
 except ImportError:
     json = __import__('json')
 
 loads = json.loads
-dumps = json.dumps
 
 JSONEncodeException = TypeError
 JSONDecodeException = ValueError
+
+
+def dumps(*args, **opts):
+    if 'default' not in opts:
+        opts['default'] = _handler
+    return json.dumps(*args, **opts)
+
+
+def _handler(obj):
+    """Convert datetime objects to ISO date strings"""
+    if isinstance(obj, datetime) and hasattr(obj, 'isoformat'):
+        return obj.isoformat()
+    else:
+        return json.dumps(obj)
