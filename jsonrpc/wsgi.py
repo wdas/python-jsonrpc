a8fa6ccd9afc56ff440467d9e4199dc53a1b4983
Neil Barber <neil.barber@disneyanimation.com>
2015-10-15 23:15:35 -0700

    wsgi: Add WsgiContentReader
    Reads data from wsgi.input in chunks
    
    Signed-off-by: Neil Barber <neil.barber@disneyanimation.com>

--- jsonrpc/wsgi.py
+++ jsonrpc/wsgi.py
@@ -15,3 +15,32 @@ class WsgiServiceHandler(ServiceHandler):
                        [('Content-Type', 'application/json'),
                         ('Content-Length', '%d' % len(result))])
         return [result]
+
+
+class WsgiContentReader(object):
+
+    def __init__(self, fp, content_length, chunk_size=4096):
+        self.fp = fp
+        self.content_length = content_length
+        self.chunk_size = chunk_size
+
+    def read_data(self):
+        result = ''
+        if self.content_length == 0:
+            return result
+
+        for chunk in self._read():
+            result += chunk
+        return result.strip()
+
+    def _read(self):
+        bytes_read = 0
+        size = self.chunk_size
+
+        while bytes_read < self.content_length:
+            remaining = self.content_length - bytes_read
+            if remaining < self.chunk_size:
+                size = remaining
+            data = self.fp.read(size)
+            bytes_read += len(data)
+            yield data
