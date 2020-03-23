e34f863ebaf451cd07addd67dd6df79ef872f14c
David Aguilar <davvid@gmail.com>
2020-03-22 17:32:08 -0700

    jsonrpc: add __all__ to appease flake8

--- jsonrpc/__init__.py
+++ jsonrpc/__init__.py
@@ -1,18 +1,43 @@
-__version__ = '2.1'
+# flake8: ignore=F401
+from __future__ import absolute_import, division, unicode_literals
 
-from jsonrpc.json import loads, dumps, JSONEncodeException, JSONDecodeException
-from jsonrpc.proxy import ServiceProxy, JSONRPCException
-from jsonrpc.servicehandler import (
+from .json import loads, dumps, JSONEncodeException, JSONDecodeException
+from .proxy import ServiceProxy, JSONRPCException
+from .servicehandler import (
     servicemethod,
     ServiceHandler,
     ServiceMethodNotFound,
     ServiceException,
 )
-from jsonrpc.cgiwrapper import handleCGI
-from jsonrpc.modpywrapper import handler
+from .cgiwrapper import handleCGI
+from .modpywrapper import handler
 
-from jsonrpc.servicehandler import servicechain
-from jsonrpc.servicehandler import servicemodule
+from .servicehandler import servicechain
+from .servicehandler import servicemodule
 
 # Backwards compatibility: remove in v2.1
-from jsonrpc.servicehandler import servicemethod as ServiceMethod
+from .servicehandler import servicemethod as ServiceMethod
+
+
+__version__ = '2.1'
+__all__ = (
+    # jsonrpc
+    'handler',
+    'handleCGI',
+    'JSONDecodeException',
+    'JSONEncodeException',
+    'JSONRPCException',
+    'servicechain',
+    'servicehandler',
+    'servicemethod',
+    'servicemodule',
+    'ServiceException',
+    'ServiceHandler',
+    'ServiceMethodNotFound',
+    'ServiceProxy',
+    # json wrappers
+    'loads',
+    'dumps',
+    # Backwards compatibility
+    'ServiceMethod',
+)
